36ac4442d2a80b8da85a419bd646fa365e541d7d
Gary Whitford <ssrdunc@gmail.com>
2022-08-16 15:19:14 -0400

    working with more work needed

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,55 @@
+# from mimetypes import guess_all_extensions
+from operator import truediv
+import random
+
+
 def play_game():
-    pass
+    name = input("What is your name?")
+        # # Here the user is asked to enter the name first
+    wanna_play = ("Do you want to play a game", name)
+    if question := "Y":
+            print("Good Luck ! ", name)
+            print("Guess a letter to try to guess the word, you get 8 guesses")
+    else:
+            print("OK, Bye")
+
+    with open('test-word.txt', 'r') as file:
+        correct_word = file.read().replace('\n', "")
+
+    right_list = []
+    wrong_list = []
+
+    guess_letters(correct_word, right_list, wrong_list)
+    # guess_letters ("airplane", right_list)  
+
+
+def guess_letters(word, right_list, wrong_list):
+    letters_in_correct_word = ["_" for letter in word]
+    # wrong_guesses = (not letters_in_correct_word)
+    # max_wrong_guesses = wrong_list +1 
+    tries = 8
+    while tries:
+        guess = input("guess a letter").lower()
+        # if max_wrong_guesses [Count(8)]:
+        #     print("You have no more guesses") 
+        # elif wrong_guesses != max_wrong_guesses:
+        #     print("Try again") 
+        # while guess_letters["> 0, < max_wrong_guesses"]:
+        #     print("Guess again")
+        #     
+        if guess not in word:
+            tries -= 1
+            wrong_list.append(guess_letters)
+            print(f'Wrong, you have {tries} more tries')
+        else:
+            for i in range(len(letters_in_correct_word)):
+                if guess == word[i]:
+                    letters_in_correct_word[i] = guess
+                    right_list.append(guess_letters)
+            print(letters_in_correct_word)
 
 
 if __name__ == "__main__":
     play_game()
+
+
---
+++ mystery_word_practice.py
@@ -0,0 +1,69 @@
+import random
+from re import M
+
+def play_game():
+    
+
+    def play_time():
+        #import words file
+        file = open("test-word.txt", "r")
+        #read the opened file
+        open_file = file.read()
+
+        # #make all the words upper case
+        words_list = open_file.upper().split()
+        
+        # #choose a random word from the list
+        mystery_word = random.choice(words_list)
+        #make blank spaces
+        blanks = (len(mystery_word)*"_")
+        #set counter for guess attempts
+        attempts = 0
+        #set list for letters user has already guessed
+        letters_guessed = []
+
+        print("Your word is", len(mystery_word), "letters long")
+        
+        while attempts < 8:
+            #increase turn number
+            attempts +=1
+            #print current letter/blank spaces, letters guessed
+            print("Guess", attempts, "of 8")
+            print("Letters already guessed", letters_guessed)
+            print(blanks)
+            #user input
+            guess = input("Guess a letter A-Z: ").upper()
+            if guess not in letters_guessed:
+                letters_guessed.append(guess)
+            #compare guess to letters in mystery_word
+            if len(guess) > 1:
+                print("One guess a time bozo\n")
+            if guess in letters_guessed and guess in mystery_word:
+                print("Ummm you already tried that\n")
+            if guess in mystery_word and guess not in letters_guessed:
+                for i in range(0, len(mystery_word)):
+                    if guess == mystery_word[i]:
+                        print("Correct\n")
+                        blanks = blanks[:i] + guess +blanks[i+1:] #replace black spaces w/ correct letters
+            if guess not in mystery_word:
+                print("Incorrect\n")
+            if "_" not in blanks:
+                print("You Win...this time....")
+                print("The word was", blanks)
+                break
+            if attempts == 8:
+                print("HAHAHAHAH YOU LOSE")
+                break
+
+    question = input("Would you like to play a game...? (Y/N): ").upper()
+    if question == "Y":
+        print("Then try to guess the word, you get 8 chances...")
+        play_time()
+    else:
+        print("Then why are you bothering me?")
+    
+
+
+#no touchy
+if __name__ == "__main__":
+    play_game()
